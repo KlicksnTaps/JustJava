999e9fe54e12c1f4a33d950be8422826c6dea5f1
MikkyHugo <mykehugo@gmail.com>
2018-01-06 17:32:12 +0100

    last commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,87 +10,135 @@
 package com.example.android.justjava;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends AppCompatActivity
-{
-    int quantity = 20;
+public class MainActivity extends AppCompatActivity {
+    int quantity = 2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view)
-    {
+    public void submitOrder(View view) {
         int price = quantity * 5;
-        displayPrice( price);
-        String pricemessage="The Total price of your service is:" + price+"/n Thankyou very much and /nplease come again soon!";
+        displayPrice(price);
+        String pricemessage = "The Total price of your service is:" + NumberFormat.getCurrencyInstance().format(price) + "\n Thankyou very much and \nplease come again soon!";
         displayMessage(pricemessage);
     }
 
     /**
-     *
      * This method is passed to the submitOrder so as to increase the button press
      */
-    public  void increment(View view)
-    {
-        quantity=quantity+1;
+    public void increment(View view) {
+        quantity = quantity + 1;
         display(quantity);
 
 
-
     }
+
     /**
      * this method is passed to the submitOrder to decrease when button is pressed
      */
-    public void decrement(View view)
-    {
-        quantity=quantity+1;
+
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
         display(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number)
-    {
+    private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
     /**
      * This method displays the value of the given price on the screen.
+     * This method is not used
      */
 
     private void displayPrice(int number)
 
     {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
         quantityTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
      * This method displays a given message on the screen.
-     *
      */
 
     private void displayMessage(String message) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
         quantityTextView.setText(message);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
