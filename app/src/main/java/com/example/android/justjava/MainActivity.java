4b54798db9e861968606bab177eff0a179825c5c
KlicksnTaps <mykehugo@gmail.com>
2018-01-23 22:20:39 +0100

    price text

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,24 +12,34 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 2;
+    public final String DEBUG_PARAM = null;
+    EditText nameText;
+    CheckBox creamCheckBox;
+    CheckBox chockBox;
+
+    String naira = "\u20A6";
+    int quantity = 0;
+    int price = 0;
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-   private GoogleApiClient client;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,33 +52,43 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method is called when the order button is clicked.
+     * the pricemessage variable immediately under will dispaly the amount with a $ the second variable displays the
+     * nairasymbol for the Nigerian Naira
      */
 
-
-
-
-
     public void submitOrder(View view) {
-       String naira= "\u20A6";
-        int price = quantity * 5;
-        displayPrice(price);
-        /**
-         * the pricemessage variable immeadiatly under will disply the amout with a $ the second variable displays the
-         * price with a N naira sign
-         */
-//        String pricemessage = "The Total price of your service is:" + NumberFormat.getCurrencyInstance().format(price) + "\n Thankyou very much and \nplease come again soon!";
-        String pricemessage = "The Total price of your service is:"+ naira+ price + "\n Thankyou very much and \nplease come again soon!";
-        displayMessage(pricemessage);
+        // price = quantity * 5;
+
+        EditText nameText = (EditText) findViewById(R.id.name_text);
+        String named = nameText.getText().toString();
+
+        CheckBox creamCheckBox = (CheckBox) findViewById(R.id.whpCream_chbox);
+        boolean cchekker = creamCheckBox.isChecked();
+
+
+        CheckBox chockBox = (CheckBox) findViewById(R.id.chocolate_text);
+        boolean cheker = chockBox.isChecked();
+
+        Log.v(DEBUG_PARAM, "just before they go");
+        displayPrice(named, cchekker, cheker, quantity);
     }
+
+
+//        String pricemessage = "The Total price of your service is:" + naira + price + "\n Thankyou very much and \nplease come again soon!";
+//        displayMessage(pricemessage);
+//    }
 
     /**
      * This method is passed to the submitOrder so as to increase the button press
      */
     public void increment(View view) {
+        if (quantity >= 100) {
+            Toast.makeText(this, "you cant have more than 100 coffes at a time", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
-
+        Log.v(DEBUG_PARAM, "you just hit me up");
 
     }
 
@@ -78,8 +98,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void decrement(View view) {
+        if (quantity <= 1) {
+
+            Toast.makeText(this, "you cant have less than one coffes at a time", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
+        Log.e(DEBUG_PARAM, "you just hit me down");
     }
 
     /**
@@ -91,17 +117,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-
      * This method displays the value of the given price on the screen.
      * This method is not used
      */
 
-    private void displayPrice(int number)
 
-    {
+    private void displayPrice(String name, boolean chk, boolean chkr, int number) {
+        name = "Hello :" + nameText.getText().toString() + "\n you ordered the following";
+        chk = creamCheckBox.isChecked();
+        if (creamCheckBox.isChecked() == true) {
+            price = price + 30;
+            name += "\n GradeOne African Cream" + naira + 30;
+
+            Log.v(DEBUG_PARAM, "creame added");
+        }
+        chkr = chockBox.isChecked();
+        if (chockBox.isChecked() == true) {
+            price = price + 100;
+            name += "\nOriginal Nigerian Chocolate : " + naira + 40;
+            Log.v(DEBUG_PARAM, "chokolate added");
+
+
+        }
+        price = price + 100;
+        name += "Highest blend of Ethiopian Coffee" + naira + 100;
+        price = price + 5;
+        price = price * number;
+
+        Log.v(DEBUG_PARAM, "total calculated");
+        name += "The total cost is :" + naira + price;
+
         TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
-        quantityTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        quantityTextView.setText(name);
+
     }
+
 
     /**
      * This method displays a given message on the screen.
@@ -109,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMessage(String message) {
         TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
+
         quantityTextView.setText(message);
     }
 }
